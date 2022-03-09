1cc4be475b8baa8221b808626145691854e99436
Miguel Losada Teixeira <mlosadateixeira@gmail.com>
2022-03-09 18:04:13 +0100

    poo

---
+++ EJERCICIOS TEMA 5/src/Electrodomesticos.java
@@ -0,0 +1,12 @@
+
+public class Electrodomesticos {
+
+	boolean comprobarConsumoEnergetico(char Letra) {
+		if (Letra == 'A' || Letra == 'B' || Letra == 'C' || Letra == 'D' || Letra == 'E' || Letra == 'F') {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+}
